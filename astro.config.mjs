2db8fc401c8515cfd6ca675a7aab97a73c519520
CTFD Admin <ethanshang@berkeley.edu>
2025-01-06 20:20:46 -0800

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'Calm-the-FI-Down.github.io',
+    base: '/Calm-the-FI-Down/'
+});
