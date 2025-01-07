414182462884d955835a4aefcbac8e0a34c389cb
CTFD Admin <ethanshang@berkeley.edu>
2025-01-06 20:32:14 -0800

    Update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'Calm-the-FI-Down.github.io'
+    site: 'https://calm-the-fi-down.github.io/'
 });
