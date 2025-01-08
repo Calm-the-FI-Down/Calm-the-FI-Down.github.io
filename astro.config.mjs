65067cf0496c7e7ae15c769b662d125de143c184
CTFD Admin <ethanshang@berkeley.edu>
2025-01-07 21:37:47 -0800

    Remove unused files and update project dependencies for React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://calm-the-fi-down.github.io/'
+  site: 'https://calm-the-fi-down.github.io/',
+  integrations: [react()]
 });
