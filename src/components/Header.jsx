eac92c36a324047df5309ed10c9d2e772d58a438
CTFD Admin <ethanshang@berkeley.edu>
2025-01-10 14:54:30 -0800

    Trying import ToDoItem from './ToDoItem.jsx';

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function HeaderComponent(){
 return (
     <a 
         href="/" 
-        class="white-space: pre-line mt-4 ml-4 bg-green-500 text-white font-bold py-2 px-3 rounded hover:bg-green-700 transition-all"
+        class="white-space:pre-line mt-4 ml-4 mb-20 bg-green-500 text-white font-bold py-2 px-3 rounded hover:bg-green-700 transition-all"
     >
         Calm the FI Down
     </a>
