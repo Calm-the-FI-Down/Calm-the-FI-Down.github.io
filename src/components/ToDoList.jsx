f31c9d95f3f8edc1eb99ee26c6c8a41abcf3ade5
CTFD Admin <ethanshang@berkeley.edu>
2025-01-11 19:50:51 -0800

    Enhance ToDoItem and ToDoList components with detailed task descriptions and title vs subtitle

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -6,75 +6,88 @@ function ToDoList() {
     const [tasks, setTasks] = useState([
         {
             id: 1,
-            text: 'Build Small Emergency Fund: Either $1000 or one months\' expenses.',
+            text: 'Build a small Emergency Fund.',
+            details: 'Either $1000 or one months\' expenses.',
             completed: false
         },
         {
             id: 2,
-            text: 'If your employer offers a 401(k) match, contribute enough to get the full match but nothing above that.',
+            text: 'Contribute enough to your 401(k) to get the full company match but nothing above that.',
+            details: 'If your employer offers a retirement match to a 401(k), 403(b), or similar.',
             completed: false
         },
         {
             id: 3,
-            text: 'Pay off all high-interest debt. I.e., anything over 10% interest.',
+            text: 'Pay off all high-interest debt.',
+            details: 'I.e., anything over 10% interest.',
             completed: false
         },
         {
             id: 4,
             text: 'Increase your emergency fund to cover 3-6 months of expenses.',
+            details: '',
             completed: false
         },
         {
             id: 5,
-            text: 'Pay off any moderate-interest debt. I.e., anything debt over 4-5% interest, excluding mortgage.',
+            text: 'Pay off any moderate-interest debt.',
+            details: 'I.e., any debt over 4-5% interest, excluding mortgage.',
             completed: false
         },
         {
             id: 6,
-            text: 'Evaluate whether to contribute to a Roth IRA or Traditional IRA and max out the yearly contributions. If you are over the income limit for a Roth IRA, consider a backdoor Roth IRA conversion.',
+            text: 'Max out the yearly contributions for your IRA or Roth IRA.',
+            details: 'Consider the tax implications when choosing between an IRA or Roth IRA. If you are over the income limit for a Roth IRA, consider a backdoor Roth IRA conversion.',
             completed: false
         },
         {
             id: 7,
-            text: 'If you are expected any large expenses in the next 5 years, save for them in a high-yield savings account.',
+            text: 'Save for any large expected expenses in the next 5 years in a high-yield savings account.',
+            details: '',
             completed: false
         },
         {
             id: 8,
-            text: 'Increase 15% of your pre-tax income to retirement savings, whether to 401(k), 403(b), Individual 401(k), SEP-IRA, or if none of the above are available, a taxable brokerage account.',
+            text: 'Increase to 15% of your pre-tax income towards retirement savings.',
+            details: 'Contribute to your 401(k), 403(b), Individual 401(k), SEP-IRA, or if none of the above are available, a taxable brokerage account.',
             completed: false
         },
         {
             id: 9,
-            text: 'If you have access to a Health Savings Account (HSA), max out your contributions.',
+            text: 'Max out your contributions to a Health Savings Account (HSA).',
+            details: 'HSAs offer triple tax benefits, but not all employers offer the high deductible health plan needed.',
             completed: false
         },
         {
             id: 10,
-            text: 'If you have children and wish to pay for some or all of their college education, contrubute to a 529 plan or similar option.',
+            text: 'Contribute to a 529 plan or similar option.',
+            details: 'If you plan to support your child with paying for education, research your state\'s plans.',
             completed: false
         },
         {
             id: 11,
             text: 'Max out 401(k), 403(b), or other retirement account contributions.',
+            details: '',
             completed: false
         },
         {
             id: 12,
-            text: 'Consider the Mega Backdoor Roth IRA if your employeer offers that plan.',
+            text: 'Consider the Mega Backdoor Roth IRA through your employer\'s retirement plan.',
+            details: 'Note: Not all employers offer this option.',
             completed: false
         },
         {
             id: 13,
             text: 'Continue to increase your savings rate to a point where you are frugal yet still spending for your values.',
+            details: '',
             completed: false
         },
         {
             id: 14,
             text: 'You\'ve successfully achieved the "boring middle"! Now, continue these habits, go enjoy life, and Calm the FI Down.',
+            details: '',
             completed: false
         }
-        
     ]);
 
     const [text, setText] = useState('');
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -12,13 +12,17 @@ function ToDoItem({task, toggleCompleted }) {
         <li
         className={`${
             task.completed ? completedStyle : notCompletedStyle
-        } min-h-28 flex flex-row justify-between items-center text-xl gap-4 px-8 py-2 cursor-pointer rounded-md shadow-md my-2 font-medium`}
+        } min-h-28 flex flex-row justify-between items-center gap-4 px-8 py-2 cursor-pointer rounded-md shadow-md my-2`}
         onClick={() => handleChange()}
         >
         <div className="bg-slate-500 text-teal-50 w-8 h-8 flex items-center rounded-full justify-center shadow-md">
             {task.id}
         </div>
-        <div className="flex-1 px-8 text-center">{task.text}</div> <div className="text-2xl">{task.completed ? "✅" : "⬜"}</div>
+        <div className="flex-1 px-8 text-center">
+            <h1 className='text-xl font-bold mb-2'>{task.text}</h1>
+            {task.details && <p className="text-s">{task.details}</p>}
+        </div> 
+        <div className="text-2xl">{task.completed ? "✅" : "⬜"}</div>
         </li>
     );
 }
