c4b05369f1603ddcce817b9adfe58a53000ba294
CTFD Admin <ethanshang@berkeley.edu>
2025-01-11 20:31:28 -0800

    Add KoFiButton component and integrate it into the checklist page

---
+++ src/components/KoFiButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function KoFiButton() {
+    return (
+        <a 
+            a target="_blank"
+            href="https://ko-fi.com/calmthefidown" 
+            class="white-space: pre-line bg-blue-500 text-white font-bold py-2 px-3 rounded hover:bg-blue-700 transition-all"
+            >
+            ☕ Support me on Ko-fi
+        </a>
+    );
+}
